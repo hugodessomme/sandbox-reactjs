28ce59433d3cc3d32b833246a98b37d2c6e52cf7
hugodessomme <hdessomme@gmail.com>
2019-03-23 13:01:18 +0100

    Update Components and props

--- js/components-props.js
+++ js/components-props.js
@@ -1,82 +1,82 @@
 /**
  * Functions and Class components
  */
-function Welcome(props) {
-    return <h1>Hello {props.name}</h1>
-}
+// function Welcome(props) {
+//     return <h1>Hello {props.name}</h1>
+// }
 
-class Welcome extends React.Component {
-    render() {
-        return <h1>Hello {this.props.name}</h1>;
-    }
-}
+// class Welcome extends React.Component {
+//     render() {
+//         return <h1>Hello {this.props.name}</h1>;
+//     }
+// }
 
 /**
  * Render a component
  */
-const element = <Welcome name="Sara"/>;
+// const element = <Welcome name="Sara"/>;
 
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// );
 
 /**
  * Composing components
  */
-function App() {
-    return (
-        <div>
-            <Welcome name="Sara"/>
-            <Welcome name="Cahel"/>
-            <Welcome name="Edit"/>
-        </div>
-    );
-}
+// function App() {
+//     return (
+//         <div>
+//             <Welcome name="Sara"/>
+//             <Welcome name="Cahel"/>
+//             <Welcome name="Edit"/>
+//         </div>
+//     );
+// }
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <App/>,
+//     document.getElementById('root')
+// );
 
 /**
  * Extracting components
  */
 // Don't do
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <div className="UserInfo">
-                <img className="Avatar"
-                    src={props.author.avatarUrl}
-                    src={props.author.name}
-                />
-                <div className="UserInfo-name">
-                    {props.author.name}
-                </div>
-            </div>
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
-}
+// function Comment(props) {
+//     return (
+//         <div className="Comment">
+//             <div className="UserInfo">
+//                 <img className="Avatar"
+//                     src={props.author.avatarUrl}
+//                     src={props.author.name}
+//                 />
+//                 <div className="UserInfo-name">
+//                     {props.author.name}
+//                 </div>
+//             </div>
+//             <div className="Comment-text">
+//                 {props.text}
+//             </div>
+//             <div className="Comment-date">
+//                 {formatDate(props.date)}
+//             </div>
+//         </div>
+//     );
+// }
 
 // Do
-function Comment(props) {
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
     return (
-        <div className="Comment">
-            <UserInfo user={props.author} />
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
+        <img 
+            className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        />
     );
 }
 
@@ -91,14 +91,37 @@ function UserInfo(props) {
     );
 }
 
-function Avatar(props) {
+function Comment(props) {
     return (
-        <img className="Avatar"
-            src={props.user.avatarUrl}
-            src={props.user.name}
-        />
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
     );
 }
+
+const comment = {
+    date: new Date(),
+    text: 'I am the comment text',
+    author: {
+        name: 'Hugo',
+        avatarUrl: 'https://picsum.photos/150/100'
+    }
+};
+
+ReactDOM.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+    />,
+    document.getElementById('root')
+);
 
 /**
  * /!\ Props are read-only /!\
